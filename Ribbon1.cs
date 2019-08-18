5e69d0eab0f6e0cc737fb1babb3ebbccbe5cb7e7
Faty-Z <54204757+Faty-Z@users.noreply.github.com>
2019-08-18 20:20:12 +0200

    Add files via upload

---
+++ Ribbon1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Word = Microsoft.Office.Interop.Word;
+using Microsoft.Office.Tools.Word;
+using System.Windows.Forms;
+
+namespace mytest
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, RibbonControlEventArgs e)
+        {
+            // To save as PDF
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.FileName = "*";
+            dlg.DefaultExt = "pdf";
+            dlg.ValidateNames = true;
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Globals.ThisAddIn.Application.ActiveDocument.ExportAsFixedFormat(dlg.FileName, Microsoft.Office.Interop.Word.WdExportFormat.wdExportFormatPDF, OpenAfterExport: true);
+            }
+        }
+
+        private void Button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            // To insert a table
+            Globals.ThisAddIn.Application.ActiveDocument.Tables.Add(Globals.ThisAddIn.Application.ActiveDocument.Range(0, 0), 3, 4);
+            Globals.ThisAddIn.Application.ActiveDocument.Tables[1].Range.Shading.BackgroundPatternColor = Microsoft.Office.Interop.Word.WdColor.wdColorSeaGreen;
+            Globals.ThisAddIn.Application.ActiveDocument.Tables[1].Range.Font.Size = 12;
+            Globals.ThisAddIn.Application.ActiveDocument.Tables[1].Rows.Borders.Enable = 1;
+        }
+
+        private void Button1_Click(object sender, RibbonControlEventArgs e)
+        {
+
+           
+
+        
+
+        }
+    }
+}
+
---
+++ ThisAddIn.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+using Microsoft.VisualStudio.Tools.Applications.Runtime;
+
+
+namespace mytest
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            this.Application.DocumentOpen += new Word.ApplicationEvents4_DocumentOpenEventHandler(WorkWithDocument);
+            ((Word.ApplicationEvents4_Event)this.Application).NewDocument += new Word.ApplicationEvents4_NewDocumentEventHandler(WorkWithDocument);
+
+            // Using InsertBefore method inserts text   
+            this.Application.ActiveDocument.Content.InsertBefore("Text @ the          Start - "); 
+            // Using InsertAfter method inserts text  
+            this.Application.ActiveDocument.Content.InsertAfter(" - Text @          the End");
+
+            // There is a second method called "selection method for inserting text after and before
+            // Using Selection Object inserting text after the text   
+            //this.Application.Selection.InsertAfter(" - Text @ the End"); 
+            // Using Selection Object inserting text before the text   
+            //this.Application.Selection.InsertBefore("Text @ the Start - ");
+
+            
+        }
+
+        private void WorkWithDocument(Microsoft.Office.Interop.Word.Document Doc)
+        {
+            // A method to add text in range
+            //Specify a range at the beginning of a document and insert the text New Text
+            // Word.Range rng = this.Application.ActiveDocument.Range(0, 0);
+            // rng.Text = "New Text";
+
+            //Select the Range object, which has expanded from one character to the length of the inserted text.
+            //rng.Select();
+
+            // To replace text in a range
+            //Word.Range rng = this.Application.ActiveDocument.Range(0, 12);
+
+            //Replace those characters with the string New Text.
+            //rng.Text = "New Text";
+
+            // Select the range
+            // rng.Select();
+
+
+
+            // Initializing the Range object  
+                Word.Range PacktRangeSelect;
+            // Check the sentence count  
+            if (this.Sentences.Count >= 1)
+            {
+             // Set the start and ent point has object       
+                object pktStartFrom = this.Sentences[2].Start;
+                object pktStopHere = this.Sentences[5].End;
+             // Assign the selection range       
+                PacktRangeSelect = this.Range(ref pktStartFrom, ref pktStopHere);
+            // Select the sentence using Select() method      
+                PacktRangeSelect.Select();
+            }
+            else
+            {
+                return;
+            }
+
+        }
+        
+
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+           
+
+            
+        }
+
+    }
+}
